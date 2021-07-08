cb296b0c2c17a3314918111f24051d410d2e334e
circlipse <87065212+circlipse@users.noreply.github.com>
2021-07-08 14:18:08 +0900

    Update Hello.java
    장나라는 내용에대해 작성

--- src/gitHello/Hello.java
+++ src/gitHello/Hello.java
@@ -5,8 +5,9 @@ public class Hello {
 		
 		System.out.println("hello");
 		
-		System.out.println("¾È³çÇÏ¼¼¿ä È«±æµ¿´Ô");
-		System.out.println("¸¸³ª¼­ ¹İ°©½À´Ï´Ù.");
-		System.out.println("¿À´ÃÀº ¸ñ¿äÀÏÀÔ´Ï´Ù.");
+		System.out.println("ì•ˆë…•í•˜ì„¸ìš” ë¯¸í•˜ë‚˜ë‹˜");
+		System.out.println("ë§Œë‚˜ì„œ ë°˜ê°‘ìŠµë‹ˆë‹¤.");
+		System.out.println("ì˜¤ëŠ˜ì€ ëª©ìš”ì¼ì…ë‹ˆë‹¤.");
+		System.out.println("ì¥ë§ˆë‚˜ëŠ”ë°...");
 	}
 }
